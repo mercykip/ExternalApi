2865d8aef6b417b7e897c9e609058af4db9ee213
mercy.kipyegon <mercy.kipyegon@metropol.co.ke>
2022-01-15 00:54:28 +0300

    update url

--- src/main/java/com/example/httprequest/controller/Country.java
+++ src/main/java/com/example/httprequest/controller/Country.java
@@ -6,16 +6,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 
 @RestController
 @RequestMapping(value = "/country")
 public class Country {
-
     @GetMapping
     public ResponseEntity<?> getCountry() {
         try {
-
             HttpRequest request = HttpRequest
                     .get("https://countriesnow.space/api/v0.1/countries")
                     .connectTimeout(120000);
@@ -25,6 +24,17 @@ public class Country {
             e.printStackTrace();
             return new ResponseEntity<>("Error!, Please try again", HttpStatus.INTERNAL_SERVER_ERROR);
         }
-
+    }
+    @GetMapping("/restCountry")
+    public ResponseEntity<?> getCountrys() {
+        try {
+            String uri="https://countriesnow.space/api/v0.1/countries";
+            RestTemplate restTemplate = new RestTemplate();
+            String result = restTemplate.getForObject(uri, String.class);
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>("Error!, Please try again", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
